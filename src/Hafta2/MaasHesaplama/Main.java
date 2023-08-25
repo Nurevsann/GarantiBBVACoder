f531c63bbeb1518e86c36eb9586d34636a638a2d
Nurevşan Kurak <92542957+Nurevsann@users.noreply.github.com>
2023-08-25 03:43:11 +0300

    MaasHesaplama

---
+++ src/Hafta2/MaasHesaplama/Main.java
@@ -0,0 +1,12 @@
+package Hafta2.MaasHesaplama;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Harry Potter", 15000, 40, 2016);
+        e1.toPrint();
+
+        Employee e2 = new Employee("Sirius Black", 17500, 40, 2012);
+        e2.toPrint();
+    }
+}
